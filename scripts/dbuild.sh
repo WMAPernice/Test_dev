3ce28d0c8983777e585d94e4953dd4643e6d6c00
Sultan Kenjeyev <ksula0155@gmail.com>
2018-07-18 15:27:01 +0000

    added launch script changed dockerfile

--- scripts/dbuild.sh
+++ scripts/dbuild.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-version=$(grep "version:" $CONFIG_FILE_PATH | cut -c 10-)
-tag=$(grep "arch: " $CONFIG_FILE_PATH | cut -c 7-)
-echo "Building version number: " $version " tag: " $tag
-
-docker build -f $DOCKER_FILE -t $DOCKER_FULL_REPO:${tag}"v"${version} .
+docker build -f ./docker/Dockerfile.fastai -t wmapernice/ynet_docker_files:$1 . 
