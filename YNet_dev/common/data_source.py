53a201a67af05626c5877729648a30ed7ffa8030
Leonard-Pernice <leonpernice@gmail.com>
2018-12-04 20:19:40 +0100

    normalization function added

--- YNet_dev/common/data_source.py
+++ YNet_dev/common/data_source.py
@@ -31,25 +31,25 @@ class Statistics:
     def per_class(zipped: zip, norm_value=65536, save_name='', inspect = False) -> dict:
         stats = {}
 
-        for t in zipped:
-            class_images = [] # bug fixed
+        for t in zipped: # zipped is the entire folder structure?
+            class_images = [] # bug fixed <- class image list reset
             for class_dir in t: # t is a tuple
                 class_name = class_dir.name
                 # read from each dir and append to the images
                 for file in class_dir.iterdir():
                     image = tiff.imread(str(file))
-                    class_images.append(image)
+                    class_images.append(image) # <- appends images in the folder for each class
 
                 # print(f"working on: {class_name}")
                 print(f"working on: {class_dir}")
-                if inspect == False:    
+                if inspect == False:
                     mean = np.mean(class_images, axis=(0, 2, 3)) / norm_value
                     stdev = np.std(class_images, axis=(0, 2, 3)) / norm_value
                 else: 
                     mean = np.mean(class_images, axis=(2, 3)) #/ norm_value
                     stdev = np.std(class_images, axis=(2, 3)) #/ norm_value
 
-                stats[class_name] = (mean, stdev)
+                stats[class_name] = (mean, stdev) # dictionary with mean and std for each class
 
         if save_name:
             Statistics.pickle(stats, save_name+".per_class.dict")  # if save is given it should be a string; empty strings are false
@@ -65,13 +65,39 @@ class Statistics:
     def per_dataset(test_dirs:[Path], train_dirs:[Path], norm_value=65536, save_name='') -> tuple:
 
         _dirs = [*test_dirs, *train_dirs]
+        
         print(len(_dirs))
+        
         images = []
         for _dir in _dirs:
             for file in _dir.iterdir():
                 if ".tif" in str(file):
                     image = tiff.imread(str(file))
                     images.append(image)
+
+        print(f"working on a dataset with length: {len(images)}")
+        mean = np.mean(images, axis=(0, 2, 3)) / norm_value
+        stdev = np.std(images, axis=(0, 2, 3)) / norm_value
+
+        stats = (mean, stdev)
+        if save_name:
+            Statistics.pickle(stats, save_name+".per_dataset.tuple")
+        return stats
+
+
+    @staticmethod
+    def one_dataset(directory:[Path], norm_value=65536, save_name='') -> tuple:
+
+        _dirs = Path(directory)
+        print(_dirs)
+        #print(len(_dirs))
+
+        images = [] #test
+        for file in _dirs.iterdir():
+            if ".tif" in str(file):
+                image = tiff.imread(str(file))
+                #print(image.shape)
+                images.append(image)
 
         print(f"working on a dataset with length: {len(images)}")
         mean = np.mean(images, axis=(0, 2, 3)) / norm_value
